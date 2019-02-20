b44ebbb4be62b9c2f4502e0ca4a854f072aad525
okan-s <okan.simsek@chasacademy.se>
2019-02-20 18:09:07 +0100

    Changed toolbar to a responsive sidebar/navbar hybrid, added/changed routes, created components for later use, imported necessary modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
+import { FavoritesComponent } from './components/user/favorites/favorites.component';
+import { LoginComponent } from './components/user/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { RegisterComponent } from './components/user/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent
+  },
+
+  {
+    path: 'recipe/:id',
+    component: RecipeDetailComponent
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'favorites',
+    component: FavoritesComponent
+  },
+
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,19 +5,47 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HeaderComponent } from './components/layout/header/header.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule, MatListModule, MatCheckboxModule, MatCardModule, MatSelectModule, MatRadioModule, MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule, MatToolbarModule } from '@angular/material';
+import { MainNavComponent } from './components/layout/main-nav/main-nav.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { RegisterComponent } from './components/user/register/register.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { FavoritesComponent } from './components/user/favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    MainNavComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RegisterComponent,
+    LoginComponent,
+    FavoritesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatSelectModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
